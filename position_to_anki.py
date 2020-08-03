47f0c739aeea06d8725f53766dfb833579efc40a
Nils Reuße <n.reusse@hxgn.net>
2020-08-03 18:54:25 +0200

    Card output as csv

--- position_to_anki.py
+++ position_to_anki.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 from bs4 import BeautifulSoup
+import csv
 import os
 import sys
 import subprocess
@@ -9,7 +10,6 @@ import tempfile
 
 def get_position(soup):
     position = soup.find_all('table')[0].find_all('td')[0].get_text('\n')
-    #position = position.decode('iso-8859-1')
     # remove emply lines at beginning/end
     position = '\n'.join([line for line in position.split('\n') if line.strip() != ''])
 
@@ -17,6 +17,9 @@ def get_position(soup):
 
 def get_analysis(soup):
     return soup.find_all('table')[2]
+
+def get_best_move(soup):
+    return get_analysis(soup).find_all('tr')[0].find_all('td')[2].get_text()
 
 def get_xgid(soup):
     return [t.get_text() for t in soup.find_all('td') if 'XGID' in t.get_text()][0].strip()
@@ -70,9 +73,25 @@ def delete_latex_files(filename):
     os.remove(f"{filename}.log")
     os.remove(f"{filename}.aux")
 
-def positions_to_anki(positions):
-    print(positions)
-    print('TODO')
+def positions_to_anki(positions, output_dir):
+    # create subfolder
+    if os.path.exists(output_dir) and not os.path.isdir(output_dir):
+        print(f"ERROR: {output_dir} exists and is not a directory")
+        sys.exit(1)
+    if not os.path.exists(output_dir):
+        os.mkdir(output_dir)
+    for number, position in positions.items():
+        with open(f"{output_dir}/{number}.txt", 'w', newline='') as csvfile:
+            # \t is the separator between front and back
+            cardwriter = csv.writer(csvfile, delimiter='\t', quotechar='"', quoting=csv.QUOTE_ALL)
+
+            cardwriter.writerow([
+                f"""<img src="{position['image']}">""",
+                f"""<center>{position['title']} - Best move: {position['best']}</center><br>
+                    {position['analysis']}
+                    <center><small>{position['xgid']}</small></center>"""
+            ])
+            # TODO: tags? -> chapter name
 
 if __name__ == '__main__':
     if len(sys.argv) <= 2:
@@ -81,6 +100,7 @@ if __name__ == '__main__':
 
     PREFIX = '501-essential-backgammon-problems'
     PNG_OUTPUT_DIR = 'png'
+    CARD_OUTPUT_DIR = 'cards'
     positions = []
     problems = {}
     
@@ -97,17 +117,19 @@ if __name__ == '__main__':
             problems[problem_number] = {
                 'image': '',
                 'analysis': get_analysis(soup),
+                'best': get_best_move(soup),
                 'xgid': get_xgid(soup),
-                'title': f"Problem {problem_number}"
+                'title': f"Problem {problem_number}",
+                'tags': 'TODO'
             }
 
     tex = positions_to_tex(positions)
     tempfile = tex_to_pdf(tex)
     pdf_to_png(tempfile, PNG_OUTPUT_DIR, PREFIX)
     delete_latex_files(tempfile)
-    print(tempfile)
 
     for problem_number in problems.keys():
-        problems[problem_number]['image'] = f"{PNG_OUTPUT_DIR}/{PREFIX}-{problem_number}.png"
+        problems[problem_number]['image'] = f"{PREFIX}-{problem_number}.png"
 
-    positions_to_anki(problems)
+    positions_to_anki(problems, CARD_OUTPUT_DIR)
+    print('move pngs into collection.media: ~/.local/share/Anki2/<User>/collection.media/')
