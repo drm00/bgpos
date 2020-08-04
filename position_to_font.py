5e39b05de3f5e00bffe61b4f15ad815b14e4a63a
Nils Reuße <n.reusse@hxgn.net>
2020-08-04 14:54:25 +0200

    Fixes, add bearoff

--- position_to_font.py
+++ position_to_font.py
@@ -1,31 +1,29 @@
 #!/usr/bin/env python3
 
-import pprint
 # gnubg position id has 14 characters
 # extreme gammon position id has 51 characters (?)
 
 XGID = '-a-B--E-B-a-dDB--b-bcb----:1:1:-1:63:0:0:0:3:8'
 XGID = '-b--B-C-CA-AdC-a-c-e-A--A-:3:-1:1:62:0:0:3:0:10'
+XGID = 'g-----E-C---fE-----b----B-:1:1:1:22:0:0:3:0:10'
+XGID = 'aEBBB-A----A------A---c-b-:1:1:1:21:0:0:3:0:10'
 
-# board consists of 11•18 cells
+# board consists of 11•19 cells
 board = [
     [' ', '\u00F1', '\u00A1', '\u00B2', '\u00B1', '\u00B2', '\u00A2', '\u00F2'],
-    [' ', '\u00F0', 'F', 'A', 'F', 'A', 'F', 'A', '\u0040', ' ', '\u0040', 'F', 'A', 'F', 'A', 'F', 'A', '\u00F0'],
-    [' ', '\u00F0', 'G', 'B', 'G', 'B', 'G', 'B', '\u0040', ' ', '\u0040', 'G', 'B', 'G', 'B', 'G', 'B', '\u00F0'],
-    [' ', '\u00F0', 'H', 'C', 'H', 'C', 'H', 'C', '\u0040', ' ', '\u0040', 'H', 'C', 'H', 'C', 'H', 'C', '\u00F0'],
-    [' ', '\u00F0', 'I', 'D', 'I', 'D', 'I', 'D', '\u0040', ' ', '\u0040', 'I', 'D', 'I', 'D', 'I', 'D', '\u00F0'],
-    [' ', '\u00F0', 'J', 'E', 'J', 'E', 'J', 'E', '\u0040', ' ', '\u0040', 'J', 'E', 'J', 'E', 'J', 'E', '\u00F0'],
-    [' ', '\u00F0', ' ', ' ', ' ', ' ', ' ', ' ', '\u0040', ' ', '\u0040', ' ', ' ', ' ', ' ', ' ', ' ', '\u00F0'],
-    [' ', '\u00F0', 'e', 'j', 'e', 'j', 'e', 'j', '\u0040', ' ', '\u0040', 'e', 'j', 'e', 'j', 'e', 'j', '\u00F0'],
-    [' ', '\u00F0', 'd', 'i', 'd', 'i', 'd', 'i', '\u0040', ' ', '\u0040', 'd', 'i', 'd', 'i', 'd', 'i', '\u00F0'],
-    [' ', '\u00F0', 'c', 'h', 'c', 'h', 'c', 'h', '\u0040', ' ', '\u0040', 'c', 'h', 'c', 'h', 'c', 'h', '\u00F0'],
-    [' ', '\u00F0', 'b', 'g', 'b', 'g', 'b', 'g', '\u0040', ' ', '\u0040', 'b', 'g', 'b', 'g', 'b', 'g', '\u00F0'],
-    [' ', '\u00F0', 'a', 'f', 'a', 'f', 'a', 'f', '\u0040', ' ', '\u0040', 'a', 'f', 'a', 'f', 'a', 'f', '\u00F0'],
+    [' ', '\u00F0', 'F', 'A', 'F', 'A', 'F', 'A', '\u0040', ' ', '\u0040', 'F', 'A', 'F', 'A', 'F', 'A', '\u00F0', ' '],
+    [' ', '\u00F0', 'G', 'B', 'G', 'B', 'G', 'B', '\u0040', ' ', '\u0040', 'G', 'B', 'G', 'B', 'G', 'B', '\u00F0', ' '],
+    [' ', '\u00F0', 'H', 'C', 'H', 'C', 'H', 'C', '\u0040', ' ', '\u0040', 'H', 'C', 'H', 'C', 'H', 'C', '\u00F0', ' '],
+    [' ', '\u00F0', 'I', 'D', 'I', 'D', 'I', 'D', '\u0040', ' ', '\u0040', 'I', 'D', 'I', 'D', 'I', 'D', '\u00F0', ' '],
+    [' ', '\u00F0', 'J', 'E', 'J', 'E', 'J', 'E', '\u0040', ' ', '\u0040', 'J', 'E', 'J', 'E', 'J', 'E', '\u00F0', ' '],
+    [' ', '\u00F0', ' ', ' ', ' ', ' ', ' ', ' ', '\u0040', ' ', '\u0040', ' ', ' ', ' ', ' ', ' ', ' ', '\u00F0', ' '],
+    [' ', '\u00F0', 'e', 'j', 'e', 'j', 'e', 'j', '\u0040', ' ', '\u0040', 'e', 'j', 'e', 'j', 'e', 'j', '\u00F0', ' '],
+    [' ', '\u00F0', 'd', 'i', 'd', 'i', 'd', 'i', '\u0040', ' ', '\u0040', 'd', 'i', 'd', 'i', 'd', 'i', '\u00F0', ' '],
+    [' ', '\u00F0', 'c', 'h', 'c', 'h', 'c', 'h', '\u0040', ' ', '\u0040', 'c', 'h', 'c', 'h', 'c', 'h', '\u00F0', ' '],
+    [' ', '\u00F0', 'b', 'g', 'b', 'g', 'b', 'g', '\u0040', ' ', '\u0040', 'b', 'g', 'b', 'g', 'b', 'g', '\u00F0', ' '],
+    [' ', '\u00F0', 'a', 'f', 'a', 'f', 'a', 'f', '\u0040', ' ', '\u0040', 'a', 'f', 'a', 'f', 'a', 'f', '\u00F0', ' '],
     [' ', '\u00F3', '\u00A3', '\u00B4', '\u00B3', '\u00B4', '\u00A4', '\u00F4'],
 ]
-
-b = [''.join(s) for s in board]
-pprint.pprint(b)
 
 def XG_get_pipcount_for_point(c):
     if c == '-':
@@ -35,7 +33,7 @@ def XG_get_pipcount_for_point(c):
 
 def set_pips(position, checkers, board):
 
-    if position < 1 or position > 24:
+    if position < 0 or position > 25:
         return False
 
     stack_height = XG_get_pipcount_for_point(checkers)
@@ -48,37 +46,24 @@ def set_pips(position, checkers, board):
     else:
         player = 'black'
 
-    indices = {
-        0: (11, 9),
-        1: (11, 16),
-        2: (11, 15),
-        3: (11, 14),
-        4: (11, 13),
-        5: (11, 12),
-        6: (11, 11),
-        7: (11, 7),
-        8: (11, 6),
-        9: (11, 5),
-        10: (11, 4),
-        11: (11, 3),
-        12: (11, 2),
-        13: (1, 2),
-        14: (1, 3),
-        15: (1, 4),
-        16: (1, 5),
-        17: (1, 6),
-        18: (1, 7),
-        19: (1, 11),
-        20: (1, 12),
-        21: (1, 13),
-        22: (1, 14),
-        23: (1, 15),
-        24: (1, 16),
-        25: (1, 9),
-    }
+    columns = [16, 15, 14, 13, 12, 11, 7, 6, 5, 4, 3, 2]
+    if position <= 12:
+        row = 11
+    else:
+        row = 1
 
-    row = indices[position][0]
-    col = indices[position][1]
+    if position == 0 or position == 25:
+        # bar
+        col = 9
+    else:
+        # regular points
+        if position > len(columns):
+            index = 24 - position
+        else:
+            index = position - 1
+
+        col = columns[index]
+
     if row == 1:
         direction = 1
     else:
@@ -88,26 +73,42 @@ def set_pips(position, checkers, board):
 
     for stack_elem in range(stack_height):
 
-        if col != 9 and stack_elem >= 5:
-            # choose approp. symbol and exit
-            pass
-
         if col == 9:
             # checkers on the bar
-            pass
-        elif position % 2 == 0:
-            # white points
-            if player == 'black':
-                board[row+(stack_elem*direction)][col] = chr(int('0x55', 16) + top_or_bottom + stack_elem)
+            if stack_elem <= 4:
+                if player == 'black':
+                    board[row+((4-stack_elem)*direction)][col] = chr(int('0xDB', 16))
+                else:
+                    board[row+((4-stack_elem)*direction)][col] = chr(int('0xD0', 16))
             else:
-                board[row+(stack_elem*direction)][col] = chr(int('0x4B', 16) + top_or_bottom + stack_elem)
+                if player == 'black':
+                    board[row][col] = chr(int('0xDB', 16) + (stack_height-5))
+                else:
+                    board[row][col] = chr(int('0xD0', 16) + (stack_height-5))
+
+                break
+
+        elif stack_elem <= 4:
+            if position % 2 == 0:
+                # white points
+                if player == 'black':
+                    board[row+(stack_elem*direction)][col] = chr(int('0x55', 16) + top_or_bottom + stack_elem)
+                else:
+                    board[row+(stack_elem*direction)][col] = chr(int('0x4B', 16) + top_or_bottom + stack_elem)
+            else:
+                # black points
+                if player == 'black':
+                    board[row+(stack_elem*direction)][col] = chr(int('0x5A', 16) + top_or_bottom + stack_elem)
+                else:
+                    board[row+(stack_elem*direction)][col] = chr(int('0x50', 16) + top_or_bottom + stack_elem)
+
         else:
-            # black points
             if player == 'black':
-                board[row+(stack_elem*direction)][col] = chr(int('0x5A', 16) + top_or_bottom + stack_elem)
+                board[row+(4*direction)][col] = chr(int('0xDB', 16) + (stack_height-5))
             else:
-                board[row+(stack_elem*direction)][col] = chr(int('0x50', 16) + top_or_bottom + stack_elem)
-    print(board)
+                board[row+(4*direction)][col] = chr(int('0xD0', 16) + (stack_height-5))
+
+            break
 
 def set_cube(value, position, board):
     value = int(value)
@@ -129,8 +130,6 @@ def set_cube(value, position, board):
 def set_turn(turn, dice, board):
     turn = int(turn)
     roll1, roll2 = dice[0], dice[1]
-    print(roll1, roll2)
-
 
     row = 6 
     if turn == 1:
@@ -138,7 +137,6 @@ def set_turn(turn, dice, board):
         col1, col2 = 12, 15
         roll1 = chr(int(int('0x37', 16) + int(roll1)))
         roll2 = chr(int(int('0x37', 16) + int(roll2)))
-        print(roll1, roll2)
     else:
         # top players turn, left side of the board
         col1, col2 = 3, 6
@@ -146,10 +144,43 @@ def set_turn(turn, dice, board):
     board[row][col1] = roll1
     board[row][col2] = roll2
 
+def set_bearoff(checkers, board):
+    for i, stack_height in enumerate(checkers):
+        if stack_height == 0:
+            continue
+
+        full, part = divmod(stack_height, 5)
+        if i == 0:
+            # top player
+            row = 1
+            direction = 1
+            diff = 0
+        else:
+            # bottom player
+            row = 11
+            direction = -1
+            diff = 5
+
+        for j in range(full):
+            board[row+(j*direction)][18] = chr(int('0xE6', 16) + diff)
+        board[row+(full*direction)][18] = chr(int('0xE6', 16) + diff + (5-part))
+
+
 if __name__ == "__main__":
     # board positions 1-24
-    for position, c in enumerate(XGID[0:26], start=1):
+    checkers = [0, 0] # top, bottom player
+    for position, c in enumerate(XGID[0:26]):
         set_pips(position, c, board)
+        i = XG_get_pipcount_for_point(c)
+        if c.islower():
+            # top player
+            checkers[0] += i
+        else:
+            checkers[1] += i
+
+    checkers[0] = 15 - checkers[0]
+    checkers[1] = 15 - checkers[1]
+    set_bearoff(checkers, board)
 
     cube_value, cube_position, turn, dice, score1, score2, crawford_jacoby, match_length, max_cube = XGID[27:].split(':')
     set_cube(cube_value, cube_position, board)
