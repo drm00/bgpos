e32e9ccee2e0e1ddf187271ba24521f0405d530b
Nils Reuße <n.reusse@hxgn.net>
2020-10-27 14:50:34 +0100

    implement parsing of gnubg position id

--- position_to_font.py
+++ position_to_font.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
 
+import base64
 import sys
 
 # gnubg position id has 14 characters
 # extreme gammon position id has 51 characters (?)
+# documentation:
+# XGID: http://www.extremegammon.com/extremegammon2.pdf, pp. 146-147
+# GnuBG Position ID: https://www.gnu.org/software/gnubg/manual/html_node/A-technical-description-of-the-Position-ID.html#A-technical-description-of-the-Position-ID
 
 XGID = '-a-B--E-B-a-dDB--b-bcb----:1:1:-1:63:0:0:0:3:8'
 XGID = '-b--B-C-CA-AdC-a-c-e-A--A-:3:-1:1:62:0:0:3:0:10'
 XGID = 'g-----E-C---fE-----b----B-:1:1:1:22:0:0:3:0:10'
-XGID = 'aFBBB-A---A-------A---c-b-:1:1:1:21:0:0:3:0:10'
+xg_to_gnubgids = {
+    'aFBBB-A---A-------A---c-b-:1:1:1:21:0:0:3:0:10': 'cwAAoN82IUAAAA:UQkFAAAAAAAA',
+}
+# AADABwEA2rYAAA:VAkSAAAAAAAA
 
 # board consists of 11•19 cells
 board = [
@@ -106,8 +113,6 @@ def set_pips(position, player, stack_height, board):
             break
 
 def set_cube(value, position, board):
-    value = int(value)
-    position = int(position)
 
     if position == -1:
         row = 1
@@ -155,7 +160,17 @@ def set_turn(turn, dice, board):
     board[row][col1] = roll1
     board[row][col2] = roll2
 
-def set_bearoff(checkers, board):
+def set_bearoff(pips, board):
+    # collect total amount of checkers on the board for each player
+    checkers = [0, 0]
+    for position, data in pips.items():
+        if data['player'] == 'top':
+            checkers[0] += data['stack']
+        else:
+            checkers[1] += data['stack']
+
+    checkers = [15-checkers[0], 15-checkers[1]]
+
     for i, stack_height in enumerate(checkers):
         if stack_height == 0:
             continue
@@ -243,47 +258,176 @@ def XG_validate_id(id):
 
     return True
 
+def XGID_to_GNUBG(xgid):
+    pass
+
+def pips_to_gnubgid(pips):
+    # assemble position id
+    # player on roll is bottom player
+    top = []
+    bottom = []
+
+    # bottom player starts
+    # pos. 25 is the bar of the bottom player
+    for position in range(1, 26):
+
+        if position not in pips or pips[position]['player'] != 'bottom':
+            bottom.append('0')
+            continue
+
+        stack = pips[position]['stack']
+        s = '1' * stack + '0'
+        bottom.append(s)
+
+    # now, lets do the same for the top player
+    # pos. 0 is the bar of the top player
+    for position in range(24, -1, -1):
+
+        if position not in pips or pips[position]['player'] != 'top':
+            top.append('0')
+            continue
+
+        stack = pips[position]['stack']
+        s = '1' * stack + '0'
+        top.append(s)
+
+    # combine them to a bitstring - gnubg emits top player first
+    bitstring = ''.join(top) + ''.join(bottom)
+    if len(bitstring) < 80:
+        bitstring += '0' * (80-len(bitstring))
+
+    all_bytes = []
+    for i in range(0, len(bitstring), 8):
+        chunk = bitstring[i:i+8][::-1]
+        byte = int(chunk, 2)
+        all_bytes.append(byte)
+    position_id = base64.b64encode(bytes(all_bytes))
+    position_id = position_id[:-2]
+
+    return position_id.decode('utf-8')
+
+def cube_exponent_to_gnubg(cube_exponent):
+    if cube_exponent > 15:
+        return False
+
+    return bin(2**cube_exponent)[2:].zfill(4)
+
+def pips_to_xgid():
+    pass
+
+def gnubg_position_id_to_bitstring(position_id):
+    if not position_id.endswith('=='):
+        position_id += '=='
+
+    key = base64.b64decode(position_id)
+    little_endian_bytes = [bin(x)[2:].zfill(8) for x in key]
+    bitstring = ''.join([b[::-1] for b in little_endian_bytes])
+
+    return bitstring
+
+def gnubg_bitstring_to_pips(bitstring):
+    """The gnubg bitstring starts with the top player, while the xgid
+    assumes the positions from the bottom players perspective.  Therefore,
+    we have to start at position 24 (from the bottom players perspective)
+    and go down until we reach the bits from the bottom player, where we
+    count up from position 1 again."""
+
+    pips = {}
+    pos = 24
+    step = -1
+    player = 'top'
+
+    for i, bit in enumerate(bitstring):
+
+        if pos == -1:
+            # switch to bottom player
+            pos = 1
+            step = 1
+            player = 'bottom'
+
+        if bit == '0':
+            pos += step
+            continue
+
+        if pos not in pips:
+            pips[pos] = {}
+        pips[pos] = {
+            'stack': pips[pos].get('stack', 0) + int(bit),
+            'player': player,
+        }
+
+    return pips
+
+def xgid_to_pips(xgid):
+    # position 0: checkers on the bar of top player
+    # board positions 1-24
+    # position 25: checkers on the bar of bottom player
+    pips = {}
+    for position, c in enumerate(xgid[0:26]):
+        stack_height = XG_get_pipcount_for_point(c)
+        if stack_height == 0:
+            continue
+        player = 'top' if c.islower() else 'bottom'
+        pips[position] = {
+            'stack': stack_height,
+            'player': player,
+        }
+
+    return pips
+
+def gnubgid_to_pips(gnubgid):
+    bitstring = gnubg_position_id_to_bitstring(id)
+    pips = gnubg_bitstring_to_pips(bitstring)
+
+    return pips
+
 if __name__ == "__main__":
     if len(sys.argv) < 2:
         print(f"USAGE: {sys.argv[0]} <position id> [--makeimg]")
         sys.exit(1)
 
+    pips = {}
     id = sys.argv[1]
     if len(id) == 14:
         # gnubg id
-        # translate to xgid? or parse?
-        print("GnuBG-IDs not supported yet")
-        sys.exit(0)
+        pips = gnubgid_to_pips(id)
+    else:
+        if not XG_validate_id(id):
+            print("ID not valid!")
+            sys.exit(1)
+        pips = xgid_to_pips(id)
 
-    if not XG_validate_id(id):
-        print("ID not valid!")
-        sys.exit(1)
+    #for pos in sorted(pips.keys()):
+        #print(f"pos {pos}: {pips[pos]}")
 
     generate_image = False
     if len(sys.argv) == 3 and sys.argv[2] == '--makeimg':
         from position_to_anki import positions_to_tex, tex_to_pdf, pdf_to_png
         generate_image = True
 
-    # board positions 1-24
-    checkers = [0, 0] # top, bottom player
-    for position, c in enumerate(id[0:26]):
-        stack_height = XG_get_pipcount_for_point(c)
-        if c.islower():
-            # top player
-            player = 'top'
-            checkers[0] += stack_height
-        else:
-            player = 'bottom'
-            checkers[1] += stack_height
-        set_pips(position, player, stack_height, board)
+    for position, data in pips.items():
+        set_pips(position, data['player'], data['stack'], board)
 
-    checkers[0] = 15 - checkers[0]
-    checkers[1] = 15 - checkers[1]
-    set_bearoff(checkers, board)
+    set_bearoff(pips, board)
 
-    cube_value, cube_position, turn, dice, score1, score2, crawford_jacoby, match_length, max_cube = id[27:].split(':')
-    set_cube(cube_value, cube_position, board)
-    set_turn(turn, dice, board)
+    # TODO also for gnubg match id
+    #cube_value, cube_position, turn, dice, score1, score2, crawford_jacoby, match_length, max_cube = id[27:].split(':')
+    #cube_value = int(cube_value)
+    #cube_position = int(cube_position)
+    #print(cube_value, cube_exponent_to_gnubg(cube_value))
+    """
+    print(match_info_to_gnubg_matchid(
+        cube_value,
+        cube_position,
+        turn,
+        crawford_jacoby,
+        dice,
+        ...
+    )
+    """
+
+    #set_cube(cube_value, cube_position, board)
+    #set_turn(turn, dice, board)
 
     #print(cube_value, cube_position, turn, dice, score1, score2, crawford_jacoby, match_length, max_cube)
 
